3ebd1e6e0ffd1cdc57b445274f4886b459f23e61
daniel <bro.danielx@gmail.com>
2019-03-01 00:16:29 -0500

    selenium part finished. scraping in progress.

---
+++ student_loans.py
@@ -0,0 +1,49 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+import os
+
+from credentials import FEDLOAN_USERNAME, FEDLOAN_PASSWORD
+
+
+
+url = "https://myfedloan.org/"
+driver = webdriver.Chrome()
+# driver.implicitly_wait(30)
+driver.get(url)
+
+sign_in_button = driver.find_element_by_class_name("flyout-control")
+sign_in_button.click()
+
+username_input = driver.find_element_by_name("username")
+username_input.send_keys(FEDLOAN_USERNAME)
+
+password_input = driver.find_element_by_name("password")
+password_input.send_keys(FEDLOAN_PASSWORD)
+
+sign_in_form_button = driver.find_element_by_name("signIn")
+sign_in_form_button.click()
+
+driver.implicitly_wait(30)
+loan_details_botton = driver.find_element_by_class_name("loanDetailsLink")
+loan_details_botton.click()
+
+links = driver.find_elements_by_tag_name("a")
+print_all_loan_details_url = "https://accountaccess.myfedloan.org/accountAccess/index.cfm?event=loan.getloanDetails&row=all&loanRegion=FD"
+print_all_loan_details_link = [link for link in links if link.get_attribute("href") == print_all_loan_details_url][0]
+print_all_loan_details_link.click()
+
+
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+loan_header_divs = soup.find_all("div", class_="loanHeader")
+number_of_loans = len(loan_header_divs)
+
+loan_detail_divs = soup.find_all("div", class_="detail")
+
+loan_status_uls = soup.find_all("ul", title="Loan Status")
+disbursement_info_uls = soup.find_all("ul", title="Disbursement Information")
+
+print(disbursement_info_uls[0].contents)
+# print([ul.getText() for ul in disbursement_info_uls])
