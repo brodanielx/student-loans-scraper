a7fef293afe7ecc2efa227c525b595305a003d6a
daniel <bro.danielx@gmail.com>
2019-03-01 07:10:14 -0500

    get loan detail ul groups

--- student_loans.py
+++ student_loans.py
@@ -9,41 +9,62 @@ from credentials import FEDLOAN_USERNAME, FEDLOAN_PASSWORD
 
 
 
-url = "https://myfedloan.org/"
-driver = webdriver.Chrome()
-# driver.implicitly_wait(30)
-driver.get(url)
+def get_html():
 
-sign_in_button = driver.find_element_by_class_name("flyout-control")
-sign_in_button.click()
+    url = "https://myfedloan.org/"
+    driver = webdriver.Chrome()
+    # driver.implicitly_wait(30)
+    driver.get(url)
 
-username_input = driver.find_element_by_name("username")
-username_input.send_keys(FEDLOAN_USERNAME)
+    sign_in_button = driver.find_element_by_class_name("flyout-control")
+    sign_in_button.click()
 
-password_input = driver.find_element_by_name("password")
-password_input.send_keys(FEDLOAN_PASSWORD)
+    username_input = driver.find_element_by_name("username")
+    username_input.send_keys(FEDLOAN_USERNAME)
 
-sign_in_form_button = driver.find_element_by_name("signIn")
-sign_in_form_button.click()
+    password_input = driver.find_element_by_name("password")
+    password_input.send_keys(FEDLOAN_PASSWORD)
 
-driver.implicitly_wait(30)
-loan_details_botton = driver.find_element_by_class_name("loanDetailsLink")
-loan_details_botton.click()
+    sign_in_form_button = driver.find_element_by_name("signIn")
+    sign_in_form_button.click()
 
-links = driver.find_elements_by_tag_name("a")
-print_all_loan_details_url = "https://accountaccess.myfedloan.org/accountAccess/index.cfm?event=loan.getloanDetails&row=all&loanRegion=FD"
-print_all_loan_details_link = [link for link in links if link.get_attribute("href") == print_all_loan_details_url][0]
-print_all_loan_details_link.click()
+    driver.implicitly_wait(30)
+    loan_details_botton = driver.find_element_by_class_name("loanDetailsLink")
+    loan_details_botton.click()
+
+    links = driver.find_elements_by_tag_name("a")
+    print_all_loan_details_url = "https://accountaccess.myfedloan.org/accountAccess/index.cfm?event=loan.getloanDetails&row=all&loanRegion=FD"
+    print_all_loan_details_link = [link for link in links if link.get_attribute("href") == print_all_loan_details_url][0]
+    print_all_loan_details_link.click()
+
+    return driver.page_source
 
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-loan_header_divs = soup.find_all("div", class_="loanHeader")
-number_of_loans = len(loan_header_divs)
+def parse_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    loan_header_divs = soup.find_all("div", class_="loanHeader")
+    number_of_loans = len(loan_header_divs)
 
-loan_detail_divs = soup.find_all("div", class_="detail")
+    loan_status_uls = soup.find_all("ul", title="Loan Status")
+    disbursement_uls = soup.find_all("ul", title="Disbursement Information")  
+    interest_rate_uls = soup.find_all("ul", title="Interest Rate Information")  
+    loan_balance_uls = soup.find_all("ul", title="Loan Balance") 
+    payment_uls = soup.find_all("ul", title="Payment Information") 
+    due_date_uls = soup.find_all("ul", title="Due Date Information")
 
-loan_status_uls = soup.find_all("ul", title="Loan Status")
-disbursement_info_uls = soup.find_all("ul", title="Disbursement Information")
+    handle_disbursement_ul(disbursement_uls[0])
 
-print(disbursement_info_uls[0].contents)
-# print([ul.getText() for ul in disbursement_info_uls])
+
+def handle_disbursement_ul(ul):
+    lis = ul.find_all("li")
+    for li in lis:
+        print(li.text)
+
+
+def get_data():
+    html = get_html()
+    parse_html(html)
+
+
+if __name__ == '__main__':
+    get_data()
