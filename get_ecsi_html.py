37b2f8c56eba816e618f343c9468bb09ddc10839
daniel <bro.danielx@gmail.com>
2019-03-22 07:43:02 -0400

    get_table() ecsi

--- get_ecsi_html.py
+++ get_ecsi_html.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
+import pandas as pd
 from pprint import pprint
 
 from credentials import ECSI_ACCOUNT_NUMBER, ECSI_PIN
@@ -12,22 +13,43 @@ from ecsi_constants import (
     student_id_input_name
 )
 
-url = "https://www.ecsi.net/cgi-bin/bcgi.exe?blogm3"
-driver = webdriver.Chrome()
-driver.get(url)
 
-student_id_input = driver.find_element_by_name(student_id_input_name)
-student_id_input.send_keys(ECSI_ACCOUNT_NUMBER)
+def get_ecsi_html():
+    url = "https://www.ecsi.net/cgi-bin/bcgi.exe?blogm3"
+    driver = webdriver.Chrome()
+    driver.get(url)
 
-pin_input = driver.find_element_by_name(pin_input_name)
-pin_input.send_keys(ECSI_PIN)
+    student_id_input = driver.find_element_by_name(student_id_input_name)
+    student_id_input.send_keys(ECSI_ACCOUNT_NUMBER)
 
-login_button = driver.find_element_by_name(login_button_name)
-login_button.click()
+    pin_input = driver.find_element_by_name(pin_input_name)
+    pin_input.send_keys(ECSI_PIN)
 
-balance_and_billing_link = driver.find_element_by_link_text(balance_and_billing_link_text)
-balance_and_billing_link.click()
+    login_button = driver.find_element_by_name(login_button_name)
+    login_button.click()
 
-current_account_balance_link = driver.find_element_by_link_text(current_account_balance_link_text)
-current_account_balance_link.click()
+    balance_and_billing_link = driver.find_element_by_link_text(balance_and_billing_link_text)
+    balance_and_billing_link.click()
 
+    current_account_balance_link = driver.find_element_by_link_text(current_account_balance_link_text)
+    current_account_balance_link.click()
+
+    return driver.page_source
+
+
+def get_table(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    table = soup.find('table', attrs={'summary': 'Current Account Balance'})
+    return table
+
+def get_data_frame_from_html_table(table):
+    data = pd.read_html(table)
+    print(data)
+
+
+
+
+if __name__ == '__main__':
+    html = get_ecsi_html()
+    table = get_table(html)
+    get_data_frame_from_html_table(table)
